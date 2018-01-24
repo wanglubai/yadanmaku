20dcabb1536923912394ee2bbecd1afe8f901e27
wangshuang <wangshuang@plu.cn>
2018-01-24 15:37:05 +0800

    添加层级弹幕 弹幕位置类型

--- README.md
+++ README.md
@@ -5,14 +5,32 @@
 初始化
 ```javascript
  var danmaku = new YaDanmaku();
-    danmaku.init({
+    /*
+    parent 容器
+    line_height 每行所占行高
+    max_layer 弹幕层数
+    max_num 弹幕队列最大值，超过这个值抛掉弹幕
+    */
+  danmaku.init({
       'parent': document.getElementById('view'),
       'gapH': 40
  });
 ```
- 
+
+```javascript
+  //参数1 弹幕在弹幕容器起始行位置比例 
+  //参数2 弹幕在弹幕容器结束行位置比例
+  //库中并没有设置 弹幕全屏 半屏 顶部等类型 ，可通过设置比例实现 0-1 
+  danmaku.setRatio(0, 1);
+```
+
+
 添加
 ```javascript
+  /*
+    msg 所显示内容支持dom和文本
+    class 在当前弹幕容器上追加class属性
+  */
   danmaku.play({
       'msg': '<font color="#666666">asas</font>'
   });
--- src/YaDanmaku.js
+++ src/YaDanmaku.js
@@ -6,14 +6,22 @@ function YaDanmaku() {
   tempThis._display = null;
   tempThis._parent = null;
   tempThis._vo = null;
-  tempThis._gapH = 40;
+  tempThis._lineHeight = 40;
   tempThis._voList = [];
   tempThis._itemList = [];
+  tempThis._layerMax = 1;
+  tempThis._startIndex = 0;
+  tempThis._endIndex = 0;
+  tempThis._maxNum = -1;
+  tempThis._startRatio = 0;
+  tempThis._endRatio = 1;
 
   tempThis.init = function (vo) {
     tempThis._vo = vo;
     tempThis._parent = vo['parent'];
-    tempThis._gapH = vo['gapH'] ? vo['gapH'] : 40;
+    tempThis._lineHeight = vo['line_height'] ? vo['line_height'] : 40;
+    tempThis._layerIndex = vo['max_layer'] ? Math.max(0, vo['max_layer'] - 1) : 0;
+    tempThis._maxNum = vo['max_num'] ? vo['max_num'] : -1;
     tempThis._display = $('<div class="YaDanmaku" style="position:relative;width:100%;height:100%;overflow:hidden"></div>');
     $(tempThis._parent).append(tempThis._display);
     tempThis._isInit = true;
@@ -24,40 +32,67 @@ function YaDanmaku() {
   }
 
   tempThis.play = function (vo) {
-    tempThis._voList.push(vo);
+    if (tempThis._maxNum == -1 || tempThis._voList.length < tempThis._maxNum) {
+      tempThis._voList.push(vo);
+    }
+  }
+
+  tempThis.setRatio = function (start, end) {
+    tempThis._startRatio = start;
+    tempThis._endRatio = end;
   }
 
   tempThis.update = function () {
     if (tempThis._isInit == false) return;
-    var maxIndex = parseInt(tempThis._display.height() / tempThis._gapH - 1);
+    if (tempThis._voList.length == 0) return;
+    var max = parseInt(tempThis._display.height() / tempThis._lineHeight - 1);
+    tempThis._startIndex = parseInt(max * tempThis._startRatio);
+    tempThis._endIndex = parseInt(max * tempThis._endRatio);
+    tempThis.updateLayer(0);
+  }
+
+  tempThis.updateLayer = function (index) {
+    if (tempThis._voList.length == 0) return;
+    if (index > tempThis._layerIndex) return;
     var curItem;
     var item;
     var vo;
-    for (var i = 0; i < maxIndex; i++) {
+    if (tempThis._itemList[index] == undefined) {
+      tempThis._itemList[index] = [];
+    }
+    var itemArr = tempThis._itemList[index];
+    var nextLayer = true;
+    for (var i = tempThis._startIndex; i < tempThis._endIndex; i++) {
       if (tempThis._voList.length == 0) return;
-      if (tempThis._itemList[i] == null) {
+      if (itemArr[i] == null) {
         vo = tempThis._voList.shift();
         vo['parent'] = tempThis._display;
-        vo['top'] = tempThis._gapH * i;
+        vo['top'] = tempThis._lineHeight * i;
         item = new YaDanmakuBase();
         item.play(vo);
-        tempThis._itemList[i] = item;
+        itemArr[i] = item;
+        nextLayer = false;
         break;
       } else {
-        curItem = tempThis._itemList[i];
+        curItem = itemArr[i];
         if (curItem.canPush()) {
           vo = tempThis._voList.shift();
           vo['parent'] = tempThis._display;
-          vo['top'] = tempThis._gapH * i;
+          vo['top'] = tempThis._lineHeight * i;
           vo['speed'] = curItem.getSpeed();
           item = new YaDanmakuBase();
           item.play(vo);
-          tempThis._itemList[i] = item;
+          itemArr[i] = item;
+          nextLayer = false;
           break;
         }
       }
     }
+    if (nextLayer && index < tempThis._layerIndex) {
+      tempThis.updateLayer(index + 1);
+    }
   }
 }
+
 module.exports = YaDanmaku;
 window['YaDanmaku'] = YaDanmaku;
